f68c4d470826d6cb9d3f9f4b4d44802317904b18
S460 <aleksandar.ta6ev@gmail.com>
2020-11-17 19:04:04 +0200

    Basic functionality is done.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myLibrary = [];
 let form = document.getElementById("form");
 let books = document.getElementById("books");
 
+
 function Book(title, author, pages, read) {
   // The Constructor
   this.title = title;
@@ -13,23 +14,25 @@ Book.prototype.displayBook = function () {
   let card = document.createElement("div");
   card.classList.add("card");
   books.appendChild(card);
-
+  
   let titleP = document.createElement("p");
   titleP.textContent = `Title: ${this.title}`;
   card.appendChild(titleP);
-
+  
   let authorP = document.createElement("p");
   authorP.textContent = `Author: ${this.author}`;
   card.appendChild(authorP);
-
+  
   let pagesP = document.createElement("p");
   pagesP.textContent = `Pages: ${this.pages}`;
   card.appendChild(pagesP);
-
+  
   let readP = document.createElement("p");
   readP.textContent = `Read: ${this.read}`;
   card.appendChild(readP);
-};
+  
+  deleteCard(card,this)
+}
 
 function addBookToLibrary() {
   let submitButton = document.getElementById("submit");
@@ -41,15 +44,19 @@ function addBookToLibrary() {
     myLibrary.push(new Book(title, author, pages, read));
     console.table(myLibrary);
     form.style.cssText = "display: none";
-    //REMOVE INNERHTML
     books.innerHTML = "";
     displayAllBooks();
+
   });
 }
+
 function showWindow() {
   let addBookButton = document.getElementById("addBook");
   addBookButton.addEventListener("click", () => {
     form.style.cssText = "display: block";
+
+    closeBtn = document.getElementById("close")
+    closeBtn.addEventListener("click", ()=> {form.style.cssText = "display: none"})
   });
 }
 
@@ -59,5 +66,24 @@ function displayAllBooks() {
   });
 }
 
+function deleteCard(card,book) {
+  let deleteBtn = document.createElement("button")
+  deleteBtn.textContent = "Remove"
+  deleteBtn.classList.add("deleteBtn")
+  card.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click",() => {
+  let num = myLibrary.indexOf(book)
+  console.log(num)
+  myLibrary.splice(num,1)
+  console.log(myLibrary)
+  books.innerHTML = ""
+  displayAllBooks()
+  })
+}
+
+
+
 addBookToLibrary();
 showWindow();
+
